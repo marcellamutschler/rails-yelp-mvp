08a121ea196330334a7abe199ba649e1d3b38cc2
Marcella Mutschler <marcella.mutschler@whu.edu>
2017-09-06 13:09:12 +0200

    done

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence:true
+  validates :rating, presence:true, numericality: { only_integer: true }, acceptance: { accept: [0, 1, 2, 3, 4, 5] }
+end
